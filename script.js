d3ecb1224cfaba0ca9fbd568b1fa26e47b93c2fd
yogaard <yoga.ardiant@gmail.com>
2023-12-21 23:18:55 +0800

    update

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika menu klik
+document.querySelector("#hamburgermenu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
 
-
+// click dimanapun menu hilang
+const hamburger = document.querySelector("#hamburgermenu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
